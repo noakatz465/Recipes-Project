8acd111b413eb50126c3a1c183b697b9d646a606
Noa Katz <noakatz465@gmail.com>
2024-11-18 15:46:28 +0200

    last

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import React from 'react'
 
-export default function Home() {
+const Page = () => {
+  const route = useRouter()
+  route.push('/pages/recipeList')
   return (
     <div>
+
     </div>
-  );
+  )
 }
+
+export default Page
